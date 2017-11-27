08e1898fd006460c2a4eda441a7a53b72c7b4187
Jaron Sampson <jaron@perfectlo.com>
2017-11-27 03:49:58 -0500

    Replaced dummy data with a database connection to SQL Server using Entity Framework

---
+++ owinAPIService/Model/ApplicationDb.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using owinAPIService.Model;
+
+namespace owinAPIService.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("MyDatabase")
+        {
+        }
+        public IDbSet<Company> Companies { get; set; }
+    }
+
+
+    public class ApplicationDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            base.Seed(context);
+            context.Companies.Add(new Company { Name = "Microsoft" });
+            context.Companies.Add(new Company { Name = "Google" });
+            context.Companies.Add(new Company { Name = "Apple" });
+        }
+    }
+}
